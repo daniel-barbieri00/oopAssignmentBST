b4d012b77276428926022b00d994e998e076ca26
Daniel <daniel.barbieri@edu.sait.ca>
2023-12-04 10:41:25 -0700

    Initial Commit

---
+++ assignment3oopBST/src/assignment3oopBST/tests.java
@@ -0,0 +1,14 @@
+package assignment3oopBST;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class tests {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
